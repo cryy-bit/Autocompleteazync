b5a8569b9c0b8930526147cdd49aa8610c7935d1
cryy-bit <77331038+cryy-bit@users.noreply.github.com>
2023-07-06 11:36:07 +0200

    Update LiveSearch.js

--- src/components/LiveSearch.js
+++ src/components/LiveSearch.js
@@ -1,19 +1,60 @@
-import React, { useState, useEffect, createElement } from "react";
+import React, { createElement, useState, useRef, useEffect } from "react";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
 import { Box } from "@mui/system";
+import debounce from "lodash.debounce";
 
 const LiveSearch = props => {
     // const dataSourceIdList = props.dataSource.items;
     let optionList = [];
 
-    const { dataSource, stringInput, dataMapping } = props;
+    const { dataSource, stringInput, dataMapping, label } = props;
+    const [isOpen, setIsOpen] = useState(false);
+    const loading = isOpen;
+    const hasChanged = useRef(false);
 
-    
-    if (dataSource.status === "available"){
-        dataSource.items.forEach(id => {
-            optionList.push(dataMapping.get(id).value);
+    const reasonEnum = {
+        SELECT: "selectOption",
+        CLEAR: "clear",
+        REMOVE: "removeOption"
+    }
+
+    const propStatus = {
+        AVAILABLE: "available",
+        LOADING:"loading"
+    }
+
+    const handleInputChange = debounce(value => {
+        stringInput.setValue(value);
+        hasChanged.current = true;
+        reloadData();
+    }, 200);
+
+    const reloadData = (reason) => {
+        if ((dataSource.status === propStatus.AVAILABLE && isOpen && hasChanged.current) || 
+        (dataSource.status === propStatus.AVAILABLE && reason === reasonEnum.CLEAR && hasChanged.current))  {
+            dataSource.reload();
+            hasChanged.current = false;
+        }
+    };
+
+    const handleOnSelect = (value, reason) => {
+        if (reason === reasonEnum.SELECT) {
+            console.warn(value);
+        }
+
+        if (reason === reasonEnum.CLEAR) {
+            hasChanged.current = true;
+            stringInput.setValue("");
+            reloadData(reason);
+        }
+    };
+
+    if (dataSource.status === propStatus.AVAILABLE) {
+        dataSource.items.map(item => {
+            let newObject = { id: item.id, companyName: dataMapping.get(item).value };
+            optionList.push(newObject);
         });
     }
 
@@ -21,32 +62,40 @@ const LiveSearch = props => {
         <Stack sx={{ width: 300 }}>
             <Autocomplete
                 id="mxAutoComplete"
-                getOptionLabel={optionList => `${optionList}`}
+                open={isOpen}
+                onOpen={() => {
+                    setIsOpen(true);
+                }}
+                onClose={() => {
+                    setIsOpen(false);
+                }}
+                onChange={(event, value, reason) => {
+                    event.preventDefault();
+                    handleOnSelect(value, reason);
+                }}
+                loading={loading}
+                loadingText={"Loading..."}
+                getOptionLabel={optionList => `${optionList.companyName}`}
+                isOptionEqualToValue={(optionList, value) => optionList.id === value.id}
                 options={optionList}
                 sx={{ width: 300 }}
                 noOptionsText={"Not found"}
                 renderOptions={(props, optionList) => (
                     <Box component="li" {...props}>
-                        {optionList}
+                        {optionList.companyName}
                     </Box>
                 )}
                 renderInput={params => (
                     <TextField
                         {...params}
                         value={stringInput.value}
-                        onChange={(event) => {
+                        onChange={event => {
                             event.preventDefault();
-                            stringInput.setValue(event.target.value);
-
-                            if (dataSource.status === "available") {
-                                console.warn(dataSource);
-                                dataSource.reload();
-                            }               
-                            console.warn(stringInput.value);
+                            handleInputChange(event.target.value);
                         }}
-                        label="Search for things"
+                        label={label}
                     >
-                        {" "}
+                        {""}
                     </TextField>
                 )}
             />
